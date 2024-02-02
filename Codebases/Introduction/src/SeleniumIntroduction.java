b5981ae72f0dca1ec3facdafeb14b453b4dc2409
Montu-Gohain <leomontugohain@gmail.com>
2024-02-02 21:01:29 +0530

    Learned how to invoke chrome browser using scripts

--- Codebases/Introduction/src/SeleniumIntroduction.java
+++ Codebases/Introduction/src/SeleniumIntroduction.java
@@ -1,7 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumIntroduction {
 	  public static void main(String[] args) {
 
-      // Step 1 : Invoking the browser
-	}
+      // Step 1 : Invoking the browser with ChromeDriver
+		  WebDriver driver = new ChromeDriver(); // Here Webdriver is an interface
+		 
+		  
+		  String SITE_URL = "https://www.udemy.com";
+		  
+		  // To hit a URL on the browser we can use this method.
+		  driver.get(SITE_URL);
+		  
+		  
+		  // To get the title of the webpage.
+		 String webpage_title = driver.getTitle();		 
+		 System.out.println(webpage_title);
+	  }
 }
