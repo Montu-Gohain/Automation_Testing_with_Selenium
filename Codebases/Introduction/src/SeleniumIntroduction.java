d1e892ddb3e708ee2e6479f6231ff13595260ee3
Montu-Gohain <leomontugohain@gmail.com>
2024-02-04 09:50:00 +0530

    Learned these basic functions of selenium
    1. How to open a browser using its driver.
    2. How to open a URL using the .get() method.
    3. How to get the title of the webpage using .getTitle() method.
    4. How to get the current URL after redirection .getCurrentUrl() method.

--- Codebases/Introduction/src/SeleniumIntroduction.java
+++ Codebases/Introduction/src/SeleniumIntroduction.java
@@ -1,29 +1,55 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class SeleniumIntroduction {
 	  public static void main(String[] args) {
+		  
+		  
+		  String TEST_URL = "http://www.coingecko.com";
+		  
+		  
        // Step 1 : Invoke Chrome browser using the ChromeBrowser Driver.
-		  WebDriver driver = new ChromeDriver();
+		  WebDriver chrome_driver = new ChromeDriver();
 		  
+	   // Todo: To Invoke Firefox we need a driver named as gecko driver
 		  // Let's try to open chrome using this script.
-		  
-		  driver.get("https://rahulshettyacademy.com");
+		  chrome_driver.get("https://rahulshettyacademy.com");
 		  
 		  
 		  // Let's get the title of the page.
-		  String page_title = driver.getTitle();
-		  System.out.println(page_title);
+		  String page_title = chrome_driver.getTitle();
+		  System.out.println("Webpage title in chrome :- " + page_title);
 		  
 		  // Let's get the current URL of the page.
 		  
-		  System.out.println(driver.getCurrentUrl());
-		  
-		  
+		  System.out.println("Currently hitting this URL :- " + chrome_driver.getCurrentUrl());
 		  
 		  // Now after running all the test, we want the browser to be automatically get closed.
 		  
-		  driver.close();
-				  
+		   //driver.close(); // This closes just the current window.
+		  
+		  // driver.quit(); closes all the windows and tabs that being opened by the selenium script.
+			
+		  chrome_driver.quit();
+		  
+		  
+		  // Todo : Let's invoke Microsoft Edge with the help of edge driver
+		  
+		  System.setProperty("webdriver.edge.driver", "E:/Exuber_work/Course_Learnings/msedgedriver.exe");
+		  WebDriver edge_driver = new EdgeDriver();
+		  
+		  edge_driver.get(TEST_URL);
+		
+		  // Let's get the title of this webpage
+		  
+		  String page_title_2 = edge_driver.getTitle();
+		  
+		  System.out.println("Webpage title in Edge :- " + page_title_2);
+		  
+		  
+		  edge_driver.close();
+		  
+		  
 	  }
 }
---
+++ Codebases/introductionselenium/src/test/java/com/qa/montuautomationlabs/AppTest.java
@@ -0,0 +1,20 @@
+package com.qa.montuautomationlabs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
