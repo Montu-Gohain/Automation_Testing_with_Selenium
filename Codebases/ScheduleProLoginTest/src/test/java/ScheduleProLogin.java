59a64bcb23c382a346f42d31d3159cc4cc7c85cd
Montu-Gohain <leomontugohain@gmail.com>
2024-02-09 12:12:00 +0530

    Added new tests in Schedule Pro login page with extent report

--- Codebases/ScheduleProLoginTest/src/test/java/ScheduleProLogin.java
+++ Codebases/ScheduleProLoginTest/src/test/java/ScheduleProLogin.java
@@ -1,6 +1,7 @@
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.File;
 import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Date;
 import java.util.Properties;
 
@@ -22,6 +24,7 @@ public class ScheduleProLogin {
         // Create dynamic filename with the timestamp.
         String filename = "target/Reports/ExtentReport-" + timestamp + ".html";
         ExtentSparkReporter spark = new ExtentSparkReporter(filename);
+        spark.config().setTheme(Theme.DARK);
         extent.attachReporter(spark);
         try {
             // Step 1: Access the testdata.properties file
@@ -40,17 +43,19 @@ public class ScheduleProLogin {
             // Test 1: Visit the URL first
             WebDriver driver = new ChromeDriver();
             driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(25));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
             ExtentTest test_visit_url = extent.createTest("Visiting Schedule Pro Login page", "Opening SchedulePro Login Page");
                 try {
                     test_visit_url.info("Opening Login page of Schedule Pro");
                     driver.get(Test_URL);
-                    Thread.sleep(20000);
+//                    Thread.sleep(20000);
                     String InPage_text_finder = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[2]/div[2]/form/h5")).getText();
                     System.out.println(InPage_text_finder);
 
                     if (InPage_text_finder.equals("Login")) {
                         test_visit_url.pass("We have reached SchedulePro Login Page");
-                        System.out.println("Test Passed");
+                        System.out.println("Test 1 Passed : We're reached the login page");
                     } else {
                         test_visit_url.fail("Failed to reach SchedulePro Login Page");
                         System.out.println("Test Failed");
@@ -58,7 +63,25 @@ public class ScheduleProLogin {
                 } catch (Exception e) {
                     e.printStackTrace();
                     System.out.println("Failed to visit Schedule pro Login page. Retrying...");
-                    test_visit_url.fail("Failed to visit SchedulePro Login page");
+                    try{
+                        test_visit_url.info("Opening Login page of Schedule Pro");
+//                        Thread.sleep(20000);
+                        String InPage_text_finder = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[2]/div[2]/form/h5")).getText();
+                        System.out.println(InPage_text_finder);
+
+                        if (InPage_text_finder.equals("Login")) {
+                            test_visit_url.pass("We have reached SchedulePro Login Page");
+                            System.out.println("Test 1 Passed : We're reached the login page");
+                        } else {
+                            test_visit_url.fail("Failed to reach SchedulePro Login Page");
+                            System.out.println("Test Failed");
+                        }
+                    }
+                    catch (Exception E){
+                        E.printStackTrace();
+                        test_visit_url.fail("Failed to visit SchedulePro Login page");
+
+                    }
                 }
 
             // Test 2: Log In Using the credentials
@@ -95,8 +118,62 @@ public class ScheduleProLogin {
                     E.printStackTrace();
                 }
 
+            // Test 3 : Logout from that dashboard page.
+
+            ExtentTest test_logout = extent.createTest("Logout from the page", "Let's logout from SchedulePro" );
+
+                try{
+                    // Click on the arrow
+                    driver.findElement(By.xpath("/html/body/div/div[2]/div/div[1]/div/ul/li[2]/i")).click();
+                    Thread.sleep(3000);
+                    // Wait for 3 seconds
+                    driver.findElement(By.xpath("/html/body/div/div[2]/div/div[1]/div/ul/li[2]/div[2]/ul/li[6]")).click();
+                    test_logout.info("Click on the Logout Option");
+                    System.out.println("Logout option clicked , confirm message should appear");
+
+                    Thread.sleep(4000);
+                    driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div[3]/div/button[2]")).click();
+                    test_logout.info("Logout Confirmation button clicked");
+
+                    Thread.sleep(7000);
+                    String final_url = driver.getCurrentUrl();
+                    System.out.println(final_url);
+
+                    if(final_url.equals(Test_URL)){
+                        test_logout.pass("Logout Test Passed");
+                    }
+                    else {
+                        test_logout.pass("Logout Test Failed");
+                    }
+                }
+                catch (Exception E){
+                    E.printStackTrace();
+                }
+
+
+            // Test 4 : Login With Remember me.
+
+            ExtentTest test_login_with_remember_me = extent.createTest("LogIn While Remember me is enabled", "LogIn with Remeber me checked");
+                try{
+//                    driver.get(Test_URL);
+                    Thread.sleep(7000);
+                    driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[2]/div[2]/form/div[3]/button")).click();
+                    test_login_with_remember_me.info("LogIn Button Clicked");
+                    Thread.sleep(17000);
+                    String current_url_ = driver.getCurrentUrl();
+                    System.out.println(current_url_ + "After running login in with Remember me.");
+                    if(current_url_.equals(predicted_url)){
+                        test_login_with_remember_me.pass("Login With Remember Me passed");
+                    }
+                    else {
+                        test_login_with_remember_me.fail("Login with remember me test failed");
+                    }
+                }catch (Exception E){
+                    E.printStackTrace();
+                }
+
             // Closing browser windows.
-            Thread.sleep(14000);
+            Thread.sleep(15000);
             driver.quit();
         } catch (Exception e) {
             e.printStackTrace();
