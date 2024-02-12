336105befa9521df5b1699c1fe3816b15261ea17
Montu-Gohain <leomontugohain@gmail.com>
2024-02-12 22:46:48 +0530

    When options are not present by default , we can
    target the common element that holds the options and store them in a
    list of webelements, after storing them we can iterate through each one
    of them and check one by one that, and click the one which we were
    searching

---
+++ Codebases/ScheduleProLoginTest/src/test/java/AutoSuggestiveDropdown.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class AutoSuggestiveDropdown {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+
+        try{
+            Properties prop = new Properties();
+            File file = new File("testdata.properties");
+            FileInputStream fis = new FileInputStream(file);
+            prop.load(fis);
+
+
+            String TEST_URL = prop.getProperty("dropdownTestingUrl");
+
+            // Step 0 : Open the browser and hit the URL
+
+            driver.get(TEST_URL);
+
+            // Step 1 : Let's put some text in the input field
+            driver.findElement(By.id("autosuggest")).sendKeys("Ind");
+
+
+            Thread.sleep(3000);
+            List<WebElement> options =  driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+
+            // Let's try to access the options
+            for(WebElement option : options){
+                if(option.getText().equalsIgnoreCase("India")){
+                    option.click();
+                    break;
+                }
+            }
+
+            // Close the browser windows after completions of all tests.
+            Thread.sleep(4000);
+            driver.quit();
+        }
+        catch (Exception E){
+            E.printStackTrace();
+        }
+    }
+}
